b3e3560de69024092c723e994ace034a0a5de08d
Abhinav <singh.abhinav6996@gmail.com>
2019-07-29 10:16:19 +0530

    exercise

--- assignments/2/index.js
+++ assignments/2/index.js
@@ -5,21 +5,17 @@
 /*** CHALLENGE 1 of 1 ***/
 
 function makePerson(name, age) {
-	// add code here
-
-
+  // add code here
+  var data = {
+    name,
+    age
+  };
+  return data;
 }
 
-var vicky = makePerson('Vicky', 24);
-
-
-// /********* Uncomment these lines to test your work! *********/
-// console.log(vicky.name); // -> Logs 'Vicky'
-// console.log(vicky.age); // -> Logs 24
-
-
-
-
+var vicky = makePerson("Vicky", 24);
+console.log(vicky.name); // -> Logs 'Vicky'
+console.log(vicky.age); // -> Logs 24
 
 /****************************************************************
                        USING OBJECT.CREATE
@@ -28,43 +24,30 @@ var vicky = makePerson('Vicky', 24);
 /*** CHALLENGE 1 of 3 ***/
 
 var personStore = {
-	// add code here
-
-
+  // add code here
 };
 
 // /********* Uncomment this line to test your work! *********/
 // personStore.greet(); // -> Logs 'hello'
 
-
-
 /*** CHALLENGE 2 of 3 ***/
 
 function personFromPersonStore(name, age) {
-	// add code here
-
-
+  // add code here
 }
 
-var sandra = personFromPersonStore('Sandra', 26);
-
+var sandra = personFromPersonStore("Sandra", 26);
 
 // /********* Uncomment these lines to test your work! *********/
 // console.log(sandra.name); // -> Logs 'Sandra'
 // console.log(sandra.age); //-> Logs 26
 // sandra.greet(); //-> Logs 'hello'
 
-
-
 /*** CHALLENGE 3 of 3 ***/
 
 // add code here
 
 // sandra.introduce(); // -> Logs 'Hi, my name is Sandra'
-
-
-
-
 
 /****************************************************************
                     USING THE 'NEW' KEYWORD
@@ -73,42 +56,30 @@ var sandra = personFromPersonStore('Sandra', 26);
 /*** CHALLENGE 1 of 3 ***/
 
 function PersonConstructor() {
-	// add code here
-
-
+  // add code here
 }
 
-
 // /********* Uncomment this line to test your work! *********/
-var simon = new PersonConstructor;
+var simon = new PersonConstructor();
 // simon.greet(); // -> Logs 'hello'
-
-
 
 /*** CHALLENGE 2 of 3 ***/
 
 function personFromConstructor(name, age) {
-	// add code here
-
-
+  // add code here
 }
 
-var mike = personFromConstructor('Mike', 30);
-
+var mike = personFromConstructor("Mike", 30);
 
 // /********* Uncomment these lines to test your work! *********/
 // console.log(mike.name); // -> Logs 'Mike'
 // console.log(mike.age); //-> Logs 30
 // mike.greet(); //-> Logs 'hello'
 
-
-
 /*** CHALLENGE 3 of 3 ***/
 // add code here
 
-
 // mike.introduce(); // -> Logs 'Hi, my name is Mike'
-
 
 /****************************************************************
                         USING ES6 CLASSES
@@ -117,34 +88,25 @@ var mike = personFromConstructor('Mike', 30);
 /*** CHALLENGE 1 of 3 ***/
 
 class PersonClass {
-	constructor() {
+  constructor() {
     // add code here
+  }
 
-
-	}
-
-	// add code here
-
+  // add code here
 }
 
-
 // /********* Uncomment this line to test your work! *********/
-var george = new PersonClass;
+var george = new PersonClass();
 // george.greet(); // -> Logs 'hello'
-
-
 
 /*** CHALLENGE 2 of 3 ***/
 
 // add code here
 
-
 // /********* Uncomment these lines to test your work! *********/
 // var thai = new DeveloperClass('Thai', 32);
 // console.log(thai.name); // -> Logs 'Thai'
 // thai.introduce(); //-> Logs 'Hello World, my name is Thai'
-
-
 
 /****************************************************************
                       EXTENSION: SUBCLASSING
@@ -154,7 +116,7 @@ var userFunctionStore = {
   sayType: function() {
     console.log("I am a " + this.type);
   }
-}
+};
 
 function userFactory(name, score) {
   let user = Object.create(userFunctionStore);
@@ -164,7 +126,7 @@ function userFactory(name, score) {
   return user;
 }
 
-var adminFunctionStore /* Put code here */ ;
+var adminFunctionStore /* Put code here */;
 
 function adminFactory(name, score) {
   // Put code here
